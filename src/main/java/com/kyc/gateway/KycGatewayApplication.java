08f5cacce3aaede6ce9ea20fc794925ba1e9b9d9
LHPH <luislyly@hotmail.com>
2024-05-29 21:18:36 -0600

    Upload first changes for this project

---
+++ src/main/java/com/kyc/gateway/KycGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kyc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycGatewayApplication.class, args);
+	}
+
+}

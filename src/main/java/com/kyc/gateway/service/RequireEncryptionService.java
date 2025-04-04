28944a1f3e7198fe0917128c717009cf05de7b99
LHPH <luislyly@hotmail.com>
2025-04-03 18:13:23 -0600

    Add logs in RequireEncryptionService

--- src/main/java/com/kyc/gateway/service/RequireEncryptionService.java
+++ src/main/java/com/kyc/gateway/service/RequireEncryptionService.java
@@ -1,6 +1,8 @@
 package com.kyc.gateway.service;
 
 import com.kyc.gateway.configuration.EncryptionControlConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class RequireEncryptionService {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(RequireEncryptionService.class);
+
     @Autowired
     private EncryptionControlConfig encryptionControlConfig;
 
     public boolean requireEncryption(ServerHttpRequest serverHttpRequest){
 
         String remoteIp = serverHttpRequest.getRemoteAddress().getAddress().getHostAddress();
+        LOGGER.info("Checking if incoming {} requires encryption",remoteIp);
+        LOGGER.info("Whitelist {}",encryptionControlConfig.getWhiteList());
         return encryptionControlConfig.isEnabled() && !encryptionControlConfig.getWhiteList().contains(remoteIp);
     }
 }
